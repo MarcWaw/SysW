e9841cc440243fb05e6f0e3273d6a7010bd63da1
Marcin Wawszczak <235274@student.pwr.edu.pl>
2020-04-25 01:03:32 +0200

    Laboratorium 4 - gotowe

--- Laboratorium4/Laboratorium4/Laboratorium4.py
+++ Laboratorium4/Laboratorium4/Laboratorium4.py
@@ -1,4 +1,31 @@
 #Autor: Marcin Wawszczak 235274
 #Program na Labolatorium 4 Systemów Wbudowanych
 
+#Zautomatyzowany ekspres do kawy, w którym można zaprogramować poszczególne parametry kawy oraz stworzyć grupę która ma obrazować produt wyjściowy -> gotową kawę
+
+
+class Kawiarka:
+
+    nazwa_kawy = []
+
+def wlacz_wylacz_podgrzewanie(self):
+    return stan
+
+def dodaj_czas_parzenia(self czas_parzenia):
+    return czas_parzenia
+
+def dodaj_temperature_wody(self, wysokosc_temperatury):
+    return temperatura
+
+def dodaj_pojemosc_naczynia(self, pojemnosc_naczynia):
+    return pojemnosc
+
+def dodaj_ilosc_mleka(self, ilosc_mleka):
+    return mleko
+
+def dodaj_ilosc_cukru(self, ilosc_cukru):
+    return cukier
+
+def dodaj_parametry_do_kawy(self, nazwa, czas_parzenia, temperatura, pojemnosc, mleko, cukier):
+    return nazwa_kawy
 
